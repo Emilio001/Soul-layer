cba3d91a8eee46d82eb89d7368da547ced036107
Emilio001 <100782766+Emilio001@users.noreply.github.com>
2026-06-30 17:31:12 +0200

    Update mintSoul.js

--- scripts/mintSoul.js
+++ scripts/mintSoul.js
@@ -1,11 +1,15 @@
 import hre from "hardhat";
 
-const CONTRACT_ADDRESS = "0x48381C8d67f6f9187d78261a5934FbbE49E06C5b";
+// Sustituye por la dirección real de tu contrato desplegado
+const CONTRACT_ADDRESS = "0x342C1023Dc80b395FB7F381C3316d75dE03303e4";
 
 async function main() {
+  const [signer] = await hre.ethers.getSigners();
   const soulToken = await hre.ethers.getContractAt("SoulToken", CONTRACT_ADDRESS);
 
-  const tx = await soulToken.mintSoul();
+  console.log("Creando identidad para:", signer.address);
+
+  const tx = await soulToken.connect(signer).mintSoul();
   await tx.wait();
 
   console.log("SoulToken creado correctamente");
